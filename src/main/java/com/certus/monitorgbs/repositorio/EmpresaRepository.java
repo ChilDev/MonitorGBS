a03fc4a25ecec69ddfd513afaa49f9af0d0ce272
Reposito01_Ejemplo <74647380@certus.edu.pe>
2023-07-15 05:55:30 -0500

    crud Empresas

---
+++ src/main/java/com/certus/monitorgbs/repositorio/EmpresaRepository.java
@@ -0,0 +1,7 @@
+package com.certus.monitorgbs.repositorio;
+
+import com.certus.monitorgbs.modelo.Empresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Integer> {
+}
